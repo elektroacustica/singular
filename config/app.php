e46aa3a8c96e94221a97011be6a4fe910ba18169
Luis Enrique G Mtz <elektroacustica@gmail.com>
2015-08-24 11:28:56 -0500

    Instalacion de Componente para las redes sociales

--- config/app.php
+++ config/app.php
@@ -143,6 +143,9 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        // Other service providers...
+        Laravel\Socialite\SocialiteServiceProvider::class,
 
     ],
 
@@ -191,6 +194,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
